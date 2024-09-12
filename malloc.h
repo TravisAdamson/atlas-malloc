f986d333c4ccde30452b26f97b98ec258a904794
Travis <travis.adamson@atlasschool.com>
2024-09-12 07:59:29 -0500

    Betty

--- malloc.h
+++ malloc.h
@@ -21,7 +21,7 @@ typedef struct header_s
 } header_t;
 
 /**
- * struct heap_data - Struct for storing heap data
+ * struct heap_s - Struct for storing heap data
  * @first_block: pointer to first block of metadata
  * @heap_size: Total size of the heap in bytes
  * @heap_free: Amount of heap free to use in bytes
